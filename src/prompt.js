39abd1e4d090d713d7c64fca985e768792897c3b
ethan-luxton <luxtonethan@hotmail.com>
2023-02-24 05:17:19 +0000

    Code cleanup and score functionality

--- src/prompt.js
+++ src/prompt.js
@@ -1,66 +1,67 @@
 const gameData = require('./game.json');
 
 const kidsroom = {
-    name: 'gameplay',
-    message: gameData.rooms.kidsroom.description.default,
-    type: 'list',
-    choices: ['Navigate', 'Search', 'Distraction'],
-    id: 'kidsroom'
-}
+  name: 'gameplay',
+  message: gameData.rooms.kidsroom.description.default,
+  type: 'list',
+  choices: ['Navigate', 'Search', 'Distraction'],
+  id: 'kidsroom',
+};
 
 const bathroom = {
   name: 'gameplay',
   message: gameData.rooms.bathroom.description.default,
   type: 'list',
   choices: ['Navigate', 'Search', 'Distraction'],
-  id: 'bathroom'
-
-}
+  id: 'bathroom',
+};
 
 const parentsroom = {
   name: 'gameplay',
   message: gameData.rooms.parentsroom.description.default,
   type: 'list',
   choices: ['Navigate', 'Search', 'Distraction'],
-  id: 'parentsroom'
-}
+  id: 'parentsroom',
+};
 
 const hallway = {
   name: 'gameplay',
   message: gameData.rooms.hallway.description.default,
   type: 'list',
   choices: ['Navigate', 'Search', 'Distraction'],
-  id: 'hallway'
-}
+  id: 'hallway',
+};
 
 const kitchen = {
   name: 'gameplay',
   message: gameData.rooms.kitchen.description.default,
   type: 'list',
   choices: ['Navigate', 'Cookie Jar'],
-  id: 'kitchen'
-
-}
+  id: 'kitchen',
+};
 
 const livingroom = {
   name: 'gameplay',
   message: gameData.rooms.livingroom.description.default,
   type: 'list',
   choices: ['Navigate', 'Search', 'Distraction'],
-  id: 'livingroom'
-
-}
+  id: 'livingroom',
+};
 
 const garage = {
   name: 'gameplay',
   message: gameData.rooms.garage.description.default,
   type: 'list',
   choices: ['Navigate', 'Search', 'Distraction'],
-  id: 'garage'
-
-}
-
+  id: 'garage',
+};
 
 module.exports = [
-    kidsroom, bathroom, parentsroom, hallway, kitchen, livingroom, garage
-]
+  kidsroom,
+  bathroom,
+  parentsroom,
+  hallway,
+  kitchen,
+  livingroom,
+  garage,
+];

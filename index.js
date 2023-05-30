15d1dcb8c22d3ee1aa0620ed1774c56a541b67aa
Ezgi <107739024+ezgi-c@users.noreply.github.com>
2023-05-30 00:08:24 -0700

    added PORT

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const { Server } = require('socket.io');
+const http = require('http');
 
-const io = new Server({
+const server = http.createServer();
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   },
 });
 
+const PORT = process.env.PORT || 4000;
 
-io.listen(4000);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const { EVENT_NAMES } = require('./src/utils');
 const inquirer = require('inquirer');

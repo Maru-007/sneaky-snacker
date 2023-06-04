ef728c895350434a57d4297d75359cacb5ea8b32
Ezgi <107739024+ezgi-c@users.noreply.github.com>
2023-06-03 23:47:07 -0700

    new openAI prompts generated for replay

--- src/prompt.js
+++ src/prompt.js
@@ -1,17 +1,7 @@
 const gameData = require('./game.json');
 const { handleApi } = require('../aiconfig');
 
-async function populateContent() {
-  for (let i = 0; i < prompts.length; i++) {
-    
-    const response = await handleApi(gameData.adjacencyList[i].id);
-    
-    responses.push(response);
-
-  }
-  console.log('Ready to start');
-}
-const responses = [];
+let responses = [];
 let prompts = []
 let kidsroom = {}
 let bathroom = {}
@@ -22,18 +12,34 @@ let livingroom = {}
 let garage = {}
 
 prompts.push(kidsroom, bathroom, parentsroom, hallway, kitchen, livingroom, garage)
+
+async function populateContent() {
+  responses = [];
+
+  for (let i = 0; i <gameData.adjacencyList.length; i++) {
+    const response = await handleApi(gameData.adjacencyList[i].id);
+    responses.push(response);
+
+  }
+  console.log('Ready to start');
+}
+
 function genPrompts () {
   prompts.forEach((room, idx) => {
     room.name = 'gameplay';
     room.message = responses[idx];
     room.type = 'list';
     room.id = gameData.adjacencyList[idx].id
+
     if (room.id === 'kitchen') {
       room.choices = ['Navigate', 'Cookie Jar'];
     } else {
       room.choices = ['Navigate', 'Search', 'Distraction'];
     };
-  })
+  });
+  
+  console.log(responses[0]);
+  console.log(prompts[0]);
 }
 
 
